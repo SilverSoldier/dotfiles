bd941e7d6a62cf17fc9abe981d545bd1f60e2342
kavya <kavya@debian-sattva>
2017-07-26 15:09:38 +0530

    added xmobarrc to configure xmobar

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -6,20 +6,51 @@ import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
 import qualified Data.Map as M
+import qualified XMonad.StackSet as W
+
+myModMask = mod1Mask
+
+myWorkspaces = ["1:web","2","3","4q","5w:mail", "6e", "7a", "8s", "9d:org"]
+myExtraWorkspaceKeys = [ (xK_q, "4q"), (xK_w, "5w:mail"), (xK_e, "6e"), (xK_a, "7a"), (xK_s, "8s"), (xK_d, "9d:org") ]
+
+myManageHook = composeAll
+	[ className =? "thunderbird" --> doShift "5w:mail" ]
+
+-- Color of current window title in xmobar.
+xmobarTitleColor = "#CEFFAC"
+
+
+-- Color of current workspace in xmobar.
+xmobarCurrentWorkspaceColor = "#CEFFAC"
+
+myAdditionalKeys =
+	[ ((0, xK_Print), spawn "urxvt")
+	, ((myModMask, xK_b), spawn "iceweasel")
+	-- Restart xmonad
+	, ((myModMask, xK_r), restart "xmonad" True)
+	] ++ [
+	  ((myModMask, key), (windows $ W.greedyView ws))
+	  | (key, ws) <- myExtraWorkspaceKeys
+	] ++ [
+	  ((myModMask .|. shiftMask, key), (windows $ W.shift ws))
+	  | (key, ws) <- myExtraWorkspaceKeys
+	]
+
 
 main = do 
+	xmproc <- spawnPipe "xmobar /home/kavya/.xmobarrc"
 	xmonad $ defaultConfig
-		{ manageHook = manageDocks <+> manageHook defaultConfig
+		{ terminal = "urxvt"
+		, modMask = myModMask
 		, layoutHook = avoidStruts  $  layoutHook defaultConfig
-		, terminal = "urxvt"
+		, manageHook = manageDocks <+> myManageHook 
+		, logHook = dynamicLogWithPP xmobarPP
+				{ ppOutput = hPutStrLn xmproc
+				, ppTitle = xmobarColor xmobarTitleColor "" . shorten 50
+				, ppCurrent = xmobarColor xmobarCurrentWorkspaceColor ""
+				, ppSep = "    "
+				}
+		, workspaces = myWorkspaces
+		} `additionalKeys`
+		myAdditionalKeys
 
-		-- keybindings
-		-- , keys = myKeys
-		}
-
--- myKeys conf@(XConfig {}) = M.fromList $
----------------------------------------------------------------
-------- Custom Key Bindings
---
-	-- [((0, xK_Print),
-	-- 	spawn $ XMonad.terminal conf)]
